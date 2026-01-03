629a326f357e999b92c640198b290891bcba6f88
Nalini Uday Timmaraju <127900389+nalini1223@users.noreply.github.com>
2026-01-02 21:54:08 -0500

    Update main.py

--- hello-fastapi/app/main.py
+++ hello-fastapi/app/main.py
@@ -1,7 +1,21 @@
+# Python
+# Import FastAPI
 from fastapi import FastAPI
 
+# Create FastAPI instance
 app = FastAPI()
 
+# Define a root endpoint that returns "Hello World"
+@app.get("/")
+def read_root():
+    return {"message": "Hello World"}
+
+#health endpoint 
 @app.get("/health")
 def health():
     return {"status": "ok"}
+
+# To run the application locally:
+# uvicorn main:app --reload
+# 'main' is the filename (main.py) and 'app' is the FastAPI instance.
+
